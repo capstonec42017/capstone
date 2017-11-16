563aeeec712f1c4060d2e3c8d9e799e998188cf7
Slattery Donohoe <slatterydonohoe@Slatterys-MacBook-Pro.local>
2017-11-16 15:40:00 -0500

    In-lab work 11_16

--- Arduino/scanse_sweep/scanse_sweep.ino
+++ Arduino/scanse_sweep/scanse_sweep.ino
@@ -3,9 +3,8 @@
 #include <Sweep.h>
 
 const uint8_t FOV = 180;
-long warnTime = 0;
 
-Sweep device(Serial);
+Sweep device(Serial1);
 
 /* 40 m, the farthest it can see. Used to compare to objects its seen so far */
 uint16_t closestDistanceInSpecifiedFOV = 40000;
@@ -19,10 +18,8 @@ float angles[500];            // in degrees (accurate to the millidegree)
 uint8_t signalStrengths[500]; // 0:255, higher is better
 uint8_t maxSyncValues = 500;
 
-// Might want to change data type to better compute velocity
 uint16_t distances[3][50];
 uint16_t times[3][50];
-uint16_t velocities[3][50];
 bool warnState = false;
 
 // Finite States for the program sequence
@@ -38,8 +35,8 @@ uint8_t currentState = STATE_WAIT_ON_RESET;
 
 void setup() {
   // Initialize serial for the sweep device
-  Serial.begin(115200); // sweep device
-  Serial1.begin(115200); // communications to steppers, not sure about port number
+  Serial.begin(9600);    // serial terminal on the computer
+  Serial1.begin(115200); // sweep device
 
   // initialize counter variables and reset the current state
   reset();
@@ -49,9 +46,11 @@ void loop() {
   switch (currentState)
   {
   case STATE_WAIT_ON_RESET:
+    Serial.print("Waiting on reset\n");
     currentState = waitOnReset() ? STATE_ADJUST_DEVICE_SETTINGS : STATE_ERROR;
     break;
   case STATE_ADJUST_DEVICE_SETTINGS:
+    Serial.print("Adjusting\n");
     currentState = adjustDeviceSettings() ? STATE_VERIFY_CURRENT_DEVICE_SETTINGS : STATE_ERROR;
     break;
   case STATE_VERIFY_CURRENT_DEVICE_SETTINGS:
@@ -66,30 +65,19 @@ void loop() {
     currentState = STATE_GATHER_DATA;
     gatherDistanceInfo();
     calculateClosingSpeeds();
-    if(checkForWarn())
-    {
-      // Only increase breaking if warning signals have gone on for more than 1.5ms
-      if(warnState && ((millis() - warnTime) > 15000) && warnTime != 0)
-      {
+    /*if (checkForWarn()) {
+      if (timer > 1500) {
         increaseBraking();
       }
-      else if((millis() - warnTime) < 15000)
-      {
-        // Make sure warn state is true and do nothing
+      else if (!warnState) {
         warnState = true;
-      }
-      // First sign of warning start timer and set warn state
-      else
-      {
-        warnTime = millis();
-        warnState = true;
+        // TODO: Start timer
       }
     }
-    else
-    {
+    else {
       warnState = false;
       decreaseBraking();
-    }
+    }*/
     break;
   case STATE_ERROR:
     // Need to add error handling
@@ -108,7 +96,7 @@ bool waitOnReset()
   {
     delay(1000);
   }
-  return device.getSampleRate() > 0;
+  return device.getMotorSpeed() > 0;
 }
 
 // Adjusts default settings
@@ -141,48 +129,42 @@ bool verifyCurrentDeviceSettings()
 // Main scanning function, perform data analysis algorithms here
 void gatherDistanceInfo()
 {
+  Serial.print("Gathering\n");
   unsigned long readTime;
   bool success = false;
-  ScanPacket last = device.getReading(success);
+  String msg;
   ScanPacket curr = device.getReading(success);
-  while (!(last.getAngleDegrees() <= beginAngle && curr.getAngleDegrees() >= beginAngle)) {
+  /*while (!(last.getAngleDegrees() <= beginAngle && curr.getAngleDegrees() >= beginAngle)) {
     last = curr;
     curr = device.getReading(success);
     readTime = millis();
-  }
+  }*/
   int currIndex = 0;
   int readIndex;
-  while (curr.getAngleDegrees() >= beginAngle || curr.getAngleDegrees() <= endAngle) {
-    readIndex = findIndex(curr.getAngleDegrees());
-    if (readIndex >= currIndex) {
-      distances[2][readIndex] = distances[1][readIndex];
-      distances[1][readIndex] = distances[0][readIndex];
-      distances[0][readIndex] = curr.getAngleDegrees();
+  //while (curr.getAngleDegrees() >= beginAngle || curr.getAngleDegrees() <= endAngle) {
+  for (int i = 1; i < 100; i++) {
+    readIndex = 0;//findIndex(curr.getAngleDegrees());
+    //if (readIndex >= currIndex) {
+//      distances[2][readIndex] = distances[1][readIndex];
+//      distances[1][readIndex] = distances[0][readIndex];
+      distances[0][0] = curr.getDistanceCentimeters();
+//
+//      times[2][readIndex] = times[1][readIndex];
+//      times[1][readIndex] = times[0][readIndex];
+//      times[0][readIndex] = readTime;
 
-      times[2][readIndex] = times[1][readIndex];
-      times[1][readIndex] = times[0][readIndex];
-      times[0][readIndex] = 
-
+      msg += "Angle: " + String(curr.getAngleDegrees(), 3) + ", Distance: " + String(distances[0][0]) + ", Index: " + String(readIndex) + "\n";
       currIndex = readIndex + 1;
-    }
+      
+    //}
     curr = device.getReading(success);
-    readTime = millis();
+    //readTime = millis();
   }
-
-
-  /******************************* OLD CODE *******************************/
-  while(!checkStopScanning())
-  {
-    // Reads while bool success is false
-    //bool success = false;
-    ScanPacket reading = device.getReading(success);
-  }
-  // Return true to get to STATE_STOP
-  return true;
+  Serial.println(msg);
 }
 
 int findIndex(float angle) {
-  return (int) angle + 75 % 360;
+  return ((int) (angle + 75) % 360) / 3;
 }
 void calculateClosingSpeeds() {
   
@@ -193,11 +175,11 @@ bool checkForWarn() {
 }
 
 void increaseBraking() {
-  Serial1.write(1);
+  // TODO: send increase braking signal
 }
 
 void decreaseBraking() {
-  Serial1.write(0);
+  // TODO: send decrease braking signal
 }
 
 // See if scan is complete
@@ -223,5 +205,6 @@ void reset()
   closestDistanceInSpecifiedFOV = 4000;
   memset(times, 0, sizeof(times));
   memset(distances, 0, sizeof(distances));
+  Serial.println("Device speed: " + String(device.getMotorSpeed()));
   device.reset();
 }
